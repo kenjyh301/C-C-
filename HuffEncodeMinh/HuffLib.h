2ed59b127f4f8b4bde7a6f66213f336c837ae779
kenjyh301 <kenjyh301@gmail.com>
2020-12-07 14:26:30 +0700

    update encode and decode function

--- HuffEncodeMinh/HuffLib.h
+++ HuffEncodeMinh/HuffLib.h
@@ -17,6 +17,11 @@
 pqueue_t* pqueue;
 uint16_t freq[256];
 
+int decode_size=0;
+int output_size;
+
+extern int Package_index;
+
 typedef struct{
 	char code;
 	int size;
@@ -73,14 +78,6 @@ set_pos(void *a, size_t pos)
 	((node_t *) a)->pos = pos;
 }
 
-//MinHeapNode* initMinHeap(int data,MinHeapNode *l, MinHeapNode *r) {
-//	MinHeapNode *ret;
-//	ret->data = data;
-//	ret->left = l;
-//	ret->right = r;
-//	return ret;
-//}
-
 node_t* initNode(pqueue_pri_t pri,char data,node_t* left,node_t* right){
 	node_t* ret=(node_t*)malloc(sizeof(node_t));
 	ret->pri=pri;
@@ -93,7 +90,7 @@ node_t* initNode(pqueue_pri_t pri,char data,node_t* left,node_t* right){
 
 void mBuildTree(int size) {
 	node_t *left, *right, *top;
-
+	int maxBranch=8;
 
 	int nodes = 0;
 	for (int i = 0; i < 256; i++) {
@@ -110,6 +107,31 @@ void mBuildTree(int size) {
 			pqueue_insert(pqueue, tmp);
 		}
 	}
+
+	while(pqueue_size(pqueue)>1){
+		int Q_size= pqueue_size(pqueue);
+		if(pqueue_size(pqueue)<=maxBranch+1) break;
+
+		int num_newNode= Q_size/2;
+		node_t** new_node= (node_t**)malloc(num_newNode*(sizeof(node_t*)));
+		for(int i=0;i<num_newNode;i++){
+//			new_node[i]= (node_t*)malloc(sizeof(node_t));
+			node_t* popNode = (node_t*)pqueue_pop(pqueue);
+			left = popNode;
+			popNode = (node_t*) pqueue_pop(pqueue);
+			right = popNode;
+			new_node[i]=initNode(left->pri+right->pri, '$', left, right);
+		}
+
+		for(int i=0;i<num_newNode;i++){
+			pqueue_insert(pqueue, new_node[i]);
+		}
+
+		maxBranch--;
+
+
+	}
+
 
 	while (pqueue_size(pqueue)>1) {
 		node_t* popNode = (node_t*)pqueue_pop(pqueue);
@@ -136,7 +158,9 @@ void printCode(node_t* topNode,char code,int size){
 		map m;
 		m.code= code;
 		m.size= size;
-		mapping[index]=m; //copy m to maaping indexth
+		mapping[index].code=m.code; //copy m to maaping indexth
+		mapping[index].size= m.size;
+		if(size>8)printf("\nsize too large %d %d",index,mapping[index].size);
 	}
 
 	if(topNode->right){
@@ -150,6 +174,7 @@ void printCode(node_t* topNode,char code,int size){
 }
 
 void createMap(){
+
 	memset(mapping,0,sizeof(map)*256);
 
 	printCode(topNode,0,0);
@@ -160,41 +185,96 @@ void shiftBitThrough(uint8_t data,uint8_t* buffer,int bit_length,int buffer_leng
 
 }
 
-void mEncode(char* s,uint8_t* output,int length){
+uint8_t* mEncode(char* s,int length){
 	int sizeInBit=0;
 	for(int i=0;i<length;i++){
-		sizeInBit+=mapping[s[i]].size;
+		int index= (uint8_t)s[i];
+		sizeInBit+=mapping[index].size;
 	}
-	int buffer_size= ceil(sizeInBit/8.0);
-	uint8_t* buffer= (uint8_t*)malloc(buffer_size*sizeof(uint8_t));
 
+	decode_size= sizeInBit;
+
+	int buffer_size= ceil(sizeInBit/8.0);
+
+	output_size= buffer_size;
+
+	uint8_t* buffer= (uint8_t*)malloc(buffer_size*sizeof(uint8_t));
+	memset(buffer,0,buffer_size*sizeof(uint8_t));
 //	int separate=0;
 
+	char* test=buffer+193;
+
 	for(int i=length-1;i>=0;i--){
-		int byte_end= sizeInBit/8;
-		int byte_start= (sizeInBit-mapping[s[i]].size)/8;
+		int byte_end= (sizeInBit-1)/8;
+		int mapping_index= (uint8_t)s[i];
+		int byte_start= (sizeInBit-mapping[mapping_index].size)/8;
 
 		if(byte_start==byte_end)// data in the same byte
 		{
-			uint8_t tmp=mapping[s[i]].code;
-			int shift= sizeInBit-mapping[s[i]].size - byte_start*8;
+			char m=s[i];
+			uint8_t tmp=mapping[mapping_index].code;
+			int shift= sizeInBit-mapping[mapping_index].size - byte_start*8;
 			tmp<<=shift;
+
+
 			buffer[byte_start]|=tmp;
+
+			sizeInBit-=mapping[mapping_index].size;
 		}
 		//data in different byte
 		else{
-			uint16_t tmp=mapping[s[i]].code;
-			int shift= sizeInBit-mapping[s[i]].size - byte_start*8;
+			char c= mapping_index;
+			uint16_t tmp=(uint8_t)mapping[mapping_index].code;
+			int shift= sizeInBit-mapping[mapping_index].size - byte_start*8;
 			tmp<<=shift;
 			buffer[byte_end]|=(uint8_t)(tmp>>8);
 			buffer[byte_start]|=(uint8_t)(tmp);
+			sizeInBit-=mapping[mapping_index].size;
 		}
 	}
 
-	for(int i=0;i<buffer_size;i++){
-		printf("%hhx   ",buffer[i]);
+//	for(int i=0;i<buffer_size;i++){
+//		printf("a%d   %hhx   ",i,buffer[i]);
+//	}
+	return buffer;
+}
+
+
+
+
+char* mDecode(uint8_t* input,int sizeInBit,int length){
+	char* answer= (char*)malloc(length);
+	int index=length;
+	node_t* itearator= topNode;
+	for(int i= sizeInBit-1;i>=0;i--){
+		int bytePos= i/8;
+		int bitPos= i-bytePos*8;
+		uint8_t check= 1<<bitPos;
+		//if bit =1
+		if(input[bytePos]&check){
+			itearator= itearator->right;
+		}else itearator= itearator->left;
+
+		if(itearator->left==NULL){
+			index--;
+			answer[index]=itearator->data;
+			itearator= topNode;
+		}
+
+		//input error check
+		if(index<0&&i>=0){
+			printf("buffer too short\n");
+			return NULL;
+		}
 	}
-	output=buffer;
+
+	if(index>0) {
+		printf("buffer define long than data  %d  %d\n",index,Package_index);
+		return NULL;
+	}
+
+	printf("Decode success");
+	return answer;
 }
 
 #endif /* HUFFLIB_H_ */
--- HuffEncodeMinh/main.c
+++ HuffEncodeMinh/main.c
@@ -11,20 +11,21 @@
 #include "HuffLib.h"
 #include "pqueue.h"
 
+uint8_t data[6120];
+FILE *f;
+int Package_index=0;
+
 void initFreq() {
-	FILE *f;
+
 	memset(freq, 0, 256 * 2);
-	f = fopen("log.log", "r");
-	if (f == NULL)
-		printf("open file not ok\n");
-	else
-		printf("open file successfully\n");
+
+
 	uint8_t header[] = { 0, 0 };
-	uint8_t data[6120];
+
 	memset(data, 0, 6120);
 	while (header[0] != 0xaa) {
 		fread(header, 2, 1, f);
-		printf("%d\n", header[0]);
+//		printf("%d\n", header[0]);
 	}
 	fread(data, 6120, 1, f);
 	uint8_t *ch;
@@ -34,11 +35,12 @@ void initFreq() {
 		//		if(freq[index]<256)
 		freq[index]++;
 	}
-	for (int i = 0; i < 256; i++) {
-		if (i % 8 == 0)
-			printf("\n");
-		printf("%d  ", (int) freq[i]);
-	}
+	Package_index++;
+//	for (int i = 0; i < 256; i++) {
+//		if (i % 8 == 0)
+//			printf("\n");
+//		printf("%d  ", (int) freq[i]);
+//	}
 }
 
 
@@ -54,14 +56,81 @@ void testCase(){
 	printf("%d  %d  %d  %d\n", mapping['a'].code, mapping['b'].code,
 			mapping['c'].code, mapping['d'].code);
 	printf("%d  %d  %d  %d\n", mapping['a'].size, mapping['b'].size,
-				mapping['c'].size, mapping['d'].size);
+			mapping['c'].size, mapping['d'].size);
+	uint8_t *output;
+	output = mEncode((char*) "abababcdca", 10);
+	char *ret;
+	ret = mDecode(output, 19, 10);
+	for (int i = 0; i < 10; i++) {
+		printf(" %c ", ret[i]);
+	}
+}
+
+void print_result(char* ret,int length){
+//	printf("\ndata in: ");
+//
+//	for (int i = 0; i < length; i++) {
+//		printf("%hhx  ", data[i]);
+//	}
+//
+//	printf("\ndata out: ");
+//
+//	for (int i = 0; i < length; i++) {
+//		printf(" %hhx ", ret[i]);
+//	}
+
+	printf("\n Efficient: %f", output_size / 6120.0 * 100.0);
 }
 
 
 int main() {
-	testCase();
-	uint8_t* output;
-	mEncode((char*)"abababcdca", output, 10);
+//	testCase();
+
+
+
+
+	f = fopen("log.log", "r");
+	if (f == NULL)
+		printf("open file not ok\n");
+	else
+		printf("open file successfully\n");
+
+for(int i=0;i<45;i++){
+	initFreq();
+
+	printf("\n");
+
+	for(int i=0;i<100;i++){
+		printf("%hhx  ",data[i]);
+	}
+	printf("\n");
+
+	mBuildTree(0);
+	createMap();
+//	for (int i = 0; i < 10; i++) {
+//		int index=(uint8_t)data[i];
+//		printf("\ncode:%hhx + size:%d    ",mapping[index].code, mapping[index].size);
+//	}
+	uint8_t *output;
+
+	int length=6120;
+	output = mEncode(data, length);
+//	printf("a%hhx  \n",output[output_size-1]);
+//	printf("a%hhx  \n",output[output_size-2]);
+//	printf("%d %d",decode_size,output_size -1);
+
+	char *ret;
+//
+//
+//	printf("")
+	ret = mDecode(output, decode_size, length);
+	if(ret==NULL)return 1;
+
+	print_result(ret,length);
+
+//	map m= mapping[1];
+//	node_t* node= topNode->left->right->right;
+}
 	return 0;
 }
 
